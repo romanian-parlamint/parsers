4a46c58bb7dfe01d7574d5511371319aa4728026
RePierre <petru.rebeja@gmail.com>
2021-02-21 17:13:00 +0200

    Added method to parse session date and type from file name

--- parse-sessions.py
+++ parse-sessions.py
@@ -1,6 +1,9 @@
 import logging
 from argparse import ArgumentParser
 from pathlib import Path
+import re
+from datetime import date
+
 
 class SessionType:
     """Encodes the values for session types.
@@ -12,6 +15,37 @@ class SessionType:
     JointVisit = 'scv'
 
 
+def parse_session_date_and_type_from_path(file_path):
+    """Parses the session date and type from file path.
+
+    Parameters
+    ----------
+    file_path: str or pathlib.Path, required
+        The path of the file containing session transcription.
+
+    Returns
+    -------
+    (session_date, session_type): tuple of (datetime.date, str)
+        The session date and its type.
+    """
+    if isinstance(file_path, Path):
+        file_path = str(file_path)
+
+    logging.info(
+        'Parsing session date and type from file name [{}]'.format(file_path))
+    match = re.search(
+        r"/(?P<year>\d{4})/(?P<month>\d{2})/(?P<type>[a-z]{1,3})(?P<day>\d{2})-(?P=month)",
+        file_path)
+    if not match:
+        logging.error(
+            "Could not parse session date and type from file [{}].".format(
+                file_path))
+        return (None, None)
+    session_date = date(int(match.group('year')), int(match.group('month')),
+                        int(match.group('day')))
+    session_type = match.group('type')
+
+    return (session_date, session_type)
 
 
 def iter_files(directory, file_type='html'):
