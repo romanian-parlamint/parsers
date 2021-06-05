455f2d3e2338848c18bae286d15168ff7d39d5fa
RePierre <petru.rebeja@gmail.com>
2021-06-05 17:15:39 +0300

    Implemented processing skeleton

--- apply-linguistic-annotation.py
+++ apply-linguistic-annotation.py
@@ -1,11 +1,60 @@
 #!/usr/bin/env python
-from lexicalanalysis import UDPipe
+from lexicalanalysis import CorpusComponentAnnotator, UDPipe
 import logging
 import argparse
+from xmlbuilder import parse_xml_file, XmlAttributes, XmlElements
+from pathlib import Path
+
+
+class CorpusIterator:
+    def __init__(self, corpus_dir, root_file):
+        """Creates a new instance of CorpusIterator.
+
+        Parameters
+        ----------
+        corpus_dir : str, required
+            The path to the corpus directory.
+        root_file : str, required
+            The name of the root file in corpus.
+        """
+        self.corpus_dir = Path(corpus_dir)
+        self.corpus_root_file = Path(self.corpus_dir, root_file)
+
+    @property
+    def root_file(self):
+        """Gets the root file of the corpus.
+
+        Returns
+        -------
+        root_file: pathlib.Path
+            The path of the root file.
+        """
+        return self.corpus_root_file
+
+    def iter_corpus_files(self):
+        """Iterates over corpus files.
+
+        """
+        for file_path in self.corpus_dir.glob("*.xml"):
+            if file_path == self.root_file:
+                continue
+            if '.ana' not in file_path.suffixes:
+                yield file_path
 
 
 def main(args):
-    pass
+    iterator = CorpusIterator(args.corpus_dir, args.root_file)
+    udpipe = UDPipe()
+    for component_file in iterator.iter_corpus_files():
+        annotator = CorpusComponentAnnotator(component_file, udpipe)
+        annotator.apply_annotation()
+        break
+    # for each segment:
+    # - call UDPipe
+    # - replace segment text with sentences and words
+    # - add segment text to CoNLL-U string
+    # save xml root to component_file.ana.xml
+    # save CoNLL-U string to component_file.conllu
 
 
 def parse_arguments():
