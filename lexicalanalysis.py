17042f93c07873440e961481a64dc52fbe5092d5
RePierre <petru.rebeja@gmail.com>
2021-06-05 17:12:31 +0300

    Added class CorpusComponentAnnotator and CoNLL-U document saving

--- lexicalanalysis.py
+++ lexicalanalysis.py
@@ -1,6 +1,8 @@
 from conllu import parse as parse_conllu
-import spacy_udpipe as sup
 import requests
+from pathlib import Path
+from xmlbuilder import parse_xml_file, save_xml, XmlAttributes, XmlElements
+import logging
 
 
 class UDPipe:
@@ -59,3 +61,123 @@ class UDPipe:
         document = response.json()
         document = parse_conllu(document['result'])
         return document
+
+
+class CorpusComponentAnnotator:
+    """Applies linguistic annotation to a corpus component file.
+    """
+    def __init__(self, component_file, udpipe):
+        """Creates a new instance of CorpusComponentAnnotator for the specified file.
+
+        Parameters
+        ----------
+        component_file : pathlib.Path
+            The path of the component file.
+        udpipe: UDPipe, required
+            The wrapper instance of UDPipe to process files.
+        """
+        self.file_name = str(component_file)
+        self.component_file = component_file
+        self.udpipe = udpipe
+        annotated_file, conllu_file = self._build_output_file_names(
+            self.component_file)
+        self.annotated_file = annotated_file
+        self.conllu_file = conllu_file
+        self.xml = parse_xml_file(self.file_name)
+        self.corpus_component = self.xml.getroot()
+        self.last_document_id = None
+        self.last_paragraph_id = None
+        self.conllu_doc = []
+
+    def apply_annotation(self):
+        """Applies linguistic annotations to the file.
+        """
+        logging.info("Annotating file {}.".format(self.file_name))
+        for seg in self.corpus_component.iterdescendants(tag=XmlElements.seg):
+            document_id = self._get_document_id(seg)
+            paragraph_id = seg.get(XmlAttributes.xml_id)
+            sentences = self.udpipe.process(seg.text)
+            # self._replace_segment_text(seg, sentences)
+            self._append_sentences_to_output(sentences, document_id,
+                                             paragraph_id)
+        # self._update_tag_usage(corpus_component)
+        # save_xml(self.xml, str(self.annotated_file))
+        self._write_conllu()
+
+    def _append_sentences_to_output(self, sentences, document_id,
+                                    paragraph_id):
+        """Appends the provided sentences to the list of CoNLL-U annotations.
+
+        Parameters
+        ----------
+        sentences : list of TokenList
+            The processed sentences.
+        document_id : str, required
+            The document id of the sentences.
+        paragraph_id : str, required
+            The paragraph id of the sentences.
+        """
+        head_sentence = sentences[0]
+        if len(self.conllu_doc) > 0:
+            head_sentence.metadata.pop('generator')
+            head_sentence.metadata.pop('udpipe_model')
+            head_sentence.metadata.pop('udpipe_model_licence')
+        if self.last_document_id != document_id:
+            head_sentence.metadata['newdoc'] = 'id = {}'.format(document_id)
+            self.last_document_id = document_id
+        else:
+            head_sentence.metadata.pop('newdoc')
+
+        if self.last_paragraph_id != paragraph_id:
+            head_sentence.metadata['newpar'] = 'id = {}'.format(paragraph_id)
+            self.last_paragraph_id = paragraph_id
+        else:
+            head_sentence.metadata.pop('newpar')
+
+        for s in sentences:
+            self.conllu_doc.append(s.serialize())
+
+    def _get_document_id(self, segment):
+        """Gets the document id for the specified segment. The document id is the id of the parent u element.
+
+        Parameters
+        ----------
+        segment : etree.Element, required
+            The segment for which to extract document id.
+
+        Returns
+        -------
+        document_id : str
+            The document id for the specified segment.
+        """
+        u = segment.getparent()
+        document_id = u.get(XmlAttributes.xml_id)
+        return document_id
+
+    def _write_conllu(self):
+        """Writes the CoNLL-U annotated sentences to a file.
+        """
+        file_name = str(self.conllu_file)
+        logging.info("Saving CoNLL-U document to {}.".format(file_name))
+        with open(file_name, 'wt', encoding='utf-8') as f:
+            for s in self.conllu_doc:
+                f.write(s)
+
+    def _build_output_file_names(self, file_path):
+        """Builds the file names for the annotated component file and CoNLL-U file.
+
+        Parameters
+        ----------
+        file_path : pathlib.Path, required
+            The path of the component file from which to infer output file names.
+
+        Returns
+        -------
+        (annotated_file, conllu_file) : tuple of (pathlib.Path, pathlib.Path)
+            The names of output files.
+        """
+        parent = file_path.parent
+        stem = file_path.stem
+        annotated_file = Path(parent, '{}.ana.xml'.format(stem))
+        conllu_file = Path(parent, '{}.conllu'.format(stem))
+        return annotated_file, conllu_file
