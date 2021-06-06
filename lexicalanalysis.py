d250ef0efd4bc37715dfe8680f45c3094a857bad
RePierre <petru.rebeja@gmail.com>
2021-06-06 06:47:31 +0300

    Moved CorpusIterator class to lexicalanalysis module

--- lexicalanalysis.py
+++ lexicalanalysis.py
@@ -6,6 +6,103 @@ import logging
 from lxml import etree
 
 
+class CorpusIterator:
+    def __init__(self, corpus_dir, root_file):
+        """Creates a new instance of CorpusIterator.
+
+        Parameters
+        ----------
+        corpus_dir : str, required
+            The path to the corpus directory.
+        root_file : str, required
+            The name of the root file in corpus.
+        """
+        self.corpus_dir = Path(corpus_dir)
+        self.corpus_root_file = Path(self.corpus_dir, root_file)
+        self.annotated_corpus_root_file = Path(
+            self.corpus_dir, "{}.ana.xml".format(self.corpus_root_file.stem))
+
+    @property
+    def root_file(self):
+        """Gets the root file of the corpus.
+
+        Returns
+        -------
+        root_file: pathlib.Path
+            The path of the root file.
+        """
+        return self.corpus_root_file
+
+    @property
+    def annotated_root_file(self):
+        """Gets the annotated root file of the corpus.
+
+        Returns
+        -------
+        annotated_root_file: pathlib.Path
+            The path of the annotated root file.
+        """
+        return self.annotated_corpus_root_file
+
+    def iter_corpus_files(self, skip_annotated=False):
+        """Iterates over corpus files.
+
+        Parameters
+        ----------
+        skip_annotated: bool, optional
+            Specifies whether to iterate over all corpus files or just the ones that are not annotated.
+            Default is False which means iterate over all corpus files.
+
+        Returns
+        -------
+        file_generator: generator of pathlib.Path
+            The generator that iterates corpus files one by one.
+        """
+        annotated_files = set()
+        if skip_annotated:
+            annotated_files = set([
+                self._get_file_name_without_extensions(f)
+                for f in self.iter_annotated_files()
+            ])
+
+        for file_path in self.corpus_dir.glob("*.xml"):
+            if file_path == self.root_file:
+                continue
+            if ('.ana' not in file_path.suffixes) and (file_path.stem
+                                                       not in annotated_files):
+                yield file_path
+
+    def iter_annotated_files(self):
+        """Iterates over annotated corpus files.
+
+        Returns
+        -------
+        file_generator: generator of pathlib.Path
+            The generator that iterates annotated corpus files one by one.
+        """
+        for file_path in self.corpus_dir.glob("*.ana.xml"):
+            if file_path == self.annotated_root_file:
+                continue
+            yield file_path
+
+    def _get_file_name_without_extensions(self, file_path):
+        """Gets the file name by replacing all extensions with empty strings.
+
+        Parameters
+        ----------
+        file_path: urllib.Path, required
+            The path of the file.
+
+        Returns
+        stem: str
+            File name without extensions.
+        """
+        stem = file_path.stem
+        for ext in file_path.suffixes:
+            stem = stem.replace(ext, '')
+        return stem
+
+
 class UDPipe:
     """Wrapper class for making requests to UDPipe API.
     """
