8ad8108a92b55d3fe5520b44c82df02d58fa13d4
RePierre <petru.rebeja@gmail.com>
2021-06-06 06:40:30 +0300

    Added support for resuming annotation process

--- apply-linguistic-annotation.py
+++ apply-linguistic-annotation.py
@@ -19,6 +19,8 @@ class CorpusIterator:
         """
         self.corpus_dir = Path(corpus_dir)
         self.corpus_root_file = Path(self.corpus_dir, root_file)
+        self.annotated_corpus_root_file = Path(
+            self.corpus_dir, "{}.ana.xml".format(self.corpus_root_file.stem))
 
     @property
     def root_file(self):
@@ -31,24 +33,85 @@ class CorpusIterator:
         """
         return self.corpus_root_file
 
-    def iter_corpus_files(self):
+    @property
+    def annotated_root_file(self):
+        """Gets the annotated root file of the corpus.
+
+        Returns
+        -------
+        annotated_root_file: pathlib.Path
+            The path of the annotated root file.
+        """
+        return self.annotated_corpus_root_file
+
+    def iter_corpus_files(self, skip_annotated=False):
         """Iterates over corpus files.
 
+        Parameters
+        ----------
+        skip_annotated: bool, optional
+            Specifies whether to iterate over all corpus files or just the ones that are not annotated.
+            Default is False which means iterate over all corpus files.
+
+        Returns
+        -------
+        file_generator: generator of pathlib.Path
+            The generator that iterates corpus files one by one.
         """
+        annotated_files = set()
+        if skip_annotated:
+            annotated_files = set([
+                self._get_file_name_without_extensions(f)
+                for f in self.iter_annotated_files()
+            ])
+
         for file_path in self.corpus_dir.glob("*.xml"):
             if file_path == self.root_file:
                 continue
-            if '.ana' not in file_path.suffixes:
+            if ('.ana' not in file_path.suffixes) and (file_path.stem
+                                                       not in annotated_files):
                 yield file_path
+
+    def iter_annotated_files(self):
+        """Iterates over annotated corpus files.
+
+        Returns
+        -------
+        file_generator: generator of pathlib.Path
+            The generator that iterates annotated corpus files one by one.
+        """
+        for file_path in self.corpus_dir.glob("*.ana.xml"):
+            if file_path == self.annotated_root_file:
+                continue
+            yield file_path
+
+    def _get_file_name_without_extensions(self, file_path):
+        """Gets the file name by replacing all extensions with empty strings.
+
+        Parameters
+        ----------
+        file_path: urllib.Path, required
+            The path of the file.
+
+        Returns
+        stem: str
+            File name without extensions.
+        """
+        stem = file_path.stem
+        for ext in file_path.suffixes:
+            stem = stem.replace(ext, '')
+        return stem
 
 
 def main(args):
     iterator = CorpusIterator(args.corpus_dir, args.root_file)
     udpipe = UDPipe()
-    for component_file in iterator.iter_corpus_files():
+    for component_file in iterator.iter_corpus_files(
+            skip_annotated=args.resume):
         annotator = CorpusComponentAnnotator(component_file, udpipe)
         annotator.apply_annotation()
-        break
+    for annotated_file in iterator.iter_annotated_files():
+        print(str(annotated_file))
     # for each segment:
     # - call UDPipe
     # - replace segment text with sentences and words
@@ -67,6 +130,11 @@ def parse_arguments():
         '--root-file',
         help="The name of the corpus root file. Default is ParlaMint-RO.xml",
         default="ParlaMint-RO.xml")
+    parser.add_argument(
+        '--resume',
+        help=
+        "Specifies whether to resume annotation or start anew. Default is start anew.",
+        action='store_true')
     parser.add_argument(
         '--root-file-template',
         help=
